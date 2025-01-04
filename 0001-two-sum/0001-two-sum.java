b321d625c523dbac22dc4c64448f3d9066f272be
Park Jinyoung (Jayon) <jayon@channel.io>
2025-01-04 16:07:37 +0900

    Time: 6 ms (52.45%), Space: 46.5 MB (7.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,20 +1,32 @@
 import java.util.*;
 
+
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] answers = new int[2];
+  public int[] twoSum(int[] nums, int target) {
+    Map<Integer, List<Integer>> numOfIndexs = new HashMap<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                int sum = nums[i] + nums[j];
-
-                if (sum == target) {
-                    answers[0] = i;
-                    answers[1] = j;
-                    break;
-                }
-            }
-        }
-        return answers;
+    for (int i = 0; i < nums.length; i++) {
+      numOfIndexs.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
     }
+
+    for (int i = 0; i < nums.length; i++) {
+      int complement = target - nums[i];
+
+      if (!numOfIndexs.containsKey(complement)) {
+        continue;
+      }
+
+      List<Integer> complementIndexs = numOfIndexs.get(complement);
+
+      if (complement == nums[i]) {
+        if (complementIndexs.size() > 1) {
+          return new int[]{i, complementIndexs.get(1)};
+        }
+        continue;
+      }
+
+      return new int[]{i, complementIndexs.get(0)};
+    }
+    return null;
+  }
 }
