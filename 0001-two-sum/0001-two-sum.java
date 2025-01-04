5e737688451d7e62c3b333f8e202468b513fa981
Park Jinyoung (Jayon) <jayon@channel.io>
2025-01-04 15:17:49 +0900

    Time: 34 ms (48.28%), Space: 45 MB (51.22%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] answers = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int sum = nums[i] + nums[j];
+
+                if (sum == target) {
+                    answers[0] = i;
+                    answers[1] = j;
+                    break;
+                }
+            }
+        }
+        return answers;
+    }
+}
