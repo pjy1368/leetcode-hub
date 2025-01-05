c9525a03d42bc66deb5fafd89bd9dfc6568494d8
Park Jinyoung (Jayon) <jayon@channel.io>
2025-01-05 15:02:23 +0900

    Time: 1 ms (20.34%), Space: 44.5 MB (44.15%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        int count = 0;
+        while (head != null) {
+            head = head.next;
+            count++;
+
+            if (count >= 20000) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
