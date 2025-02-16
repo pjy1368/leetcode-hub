19c94b2cc380cda718df676a705743f669d8fbff
Park Jinyoung (Jayon) <jayon@channel.io>
2025-02-16 22:46:52 +0900

    Time: 23 ms (88.11%), Space: 41.9 MB (93.33%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -12,7 +12,7 @@ class Solution {
 
         for (int num : nums) {
             for (int i = target; i >= num; i--) {
-                dp[i] = dp[i - num];
+                dp[i] = dp[i] || dp[i - num];
             }
         }
         return dp[target];
