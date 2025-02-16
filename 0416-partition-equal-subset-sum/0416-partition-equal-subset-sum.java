7afeace8ca11c187bc1ceed102976024a112b898
Park Jinyoung (Jayon) <jayon@channel.io>
2025-02-16 22:31:46 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int total = Arrays.stream(nums).sum();
+
+        if (total % 2 != 0) {
+            return false;
+        }
+
+        int target = total / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int i = target; i >= num; i--) {
+                dp[i] = dp[i - num];
+            }
+        }
+        return dp[target];
+    }
+}
