15711f55d20268223761a5a5e49198dd4baad779
Park Jinyoung (Jayon) <jayon@channel.io>
2025-02-18 01:16:32 +0900

    Time: 150 ms (31.4%), Space: 47 MB (7.29%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -2,7 +2,6 @@ class Solution {
     public String longestPalindrome(String s) {
         boolean[][] dp = new boolean[s.length()][s.length()];
 
-        int answerSize = Integer.MIN_VALUE;
         String answer = "";
         for (int len = 1; len <= s.length(); len++) {
             for (int i = 0; i <= s.length() - len; i++) {
@@ -10,23 +9,20 @@ class Solution {
 
                 if (len == 1) {
                     dp[i][j] = true;
-                    if (answerSize < len) {
-                        answerSize = Math.max(answerSize, len);
+                    if (answer.length() < len) {
                         answer = s.substring(i, j + 1);
                     }
                 } else if (len == 2) {
                     if (s.charAt(i) == s.charAt(j)) {
                         dp[i][j] = true;
-                        if (answerSize < len) {
-                            answerSize = Math.max(answerSize, len);
+                        if (answer.length() < len) {
                             answer = s.substring(i, j + 1);
                         }
                     }
                 } else {
                     if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
                         dp[i][j] = true;
-                        if (answerSize < len) {
-                            answerSize = Math.max(answerSize, len);
+                        if (answer.length() < len) {
                             answer = s.substring(i, j + 1);
                         }
                     }
