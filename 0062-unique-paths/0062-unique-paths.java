d430e57b35b83e87ad944c33d70d8d87441e62f0
Park Jinyoung (Jayon) <jayon@channel.io>
2025-02-23 21:36:01 +0900

    Time: 0 ms (100%), Space: 40.4 MB (59.54%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        dp[1][0] = 1;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+            }
+        }
+        return dp[m][n];
+    }
+}
